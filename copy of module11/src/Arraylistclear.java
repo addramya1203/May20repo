c0fb4bb5e8f5808664e7b183e75730600b047b19
RamyaAddepalli <ramya.addepalli1203@gmail.com>
2019-05-20 17:43:51 +0530

    Module11 commit

---
+++ copy of module11/src/Arraylistclear.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Arraylistclear {
+
+	public static void main(String[] args) {
+
+
+		ArrayList<Integer> arli=new ArrayList<Integer>();
+		arli.add(12);
+		arli.add(17);
+		arli.add(89);
+		arli.add(9);
+		arli.add(2);
+		System.out.println(arli);
+		
+		
+	}
+
+}
---
+++ copy of module11/src/removeduplicatesfromarray.java
@@ -0,0 +1,10 @@
+
+public class removeduplicatesfromarray {
+
+	int a[] = { 4, 23, 55, 8, 9,9,4,5,5,6, 50, 80, 7 };
+	
+	public static void main(String args[]) {
+		
+	}
+
+}
